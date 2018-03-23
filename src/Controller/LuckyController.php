61aac3db2fe80aa858e3bb47edca63a904fa7855
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2018-03-22 23:23:19 -0300

    Mais aprendizado

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -1,20 +1,46 @@
 <?php
-
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
-class LuckyController extends Controller {
+class LuckyController extends Controller
+{
 
-	/**
-	 *
-	 * @Symfony\Component\Routing\Annotation\Route("/lucky/number")
-	 */
-	public function number() {
-		$number = mt_rand(0, 100);
-		
-		return $this->render('lucky/number.html.twig', array (
-				'number' => $number 
-		));
-	}
+    /**
+     *
+     * @Route("/lucky/number")
+     */
+    public function number()
+    {
+        $number = mt_rand(0, 100);
+        
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number
+        ));
+    }
+
+    /**
+     *
+     * @Route("/jzon")
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function jzon()
+    {
+        return $this->json(array(
+            'username' => 'jane.doe'
+        ));
+    }
+
+    /**
+     * @Route("/pdfme")
+     * 
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
+    public function pdfme()
+    {
+        return $this->file('/usr/share/doc/qpdf/qpdf-manual.pdf', 'bla.pdf', ResponseHeaderBag::DISPOSITION_INLINE);
+    }
 }
