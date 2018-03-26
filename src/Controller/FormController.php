735a41f62cd5eb25d782d62ef15902e4f9e581e3
Carlos Eduardo Pauluk <carlospauluk@gmail.com>
2018-03-26 09:26:58 -0300

    Mais testes.

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -9,17 +9,23 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class DefaultController extends Controller {
+use Symfony\Component\Routing\Annotation\Route;
 
+class FormController extends Controller {
+
+    /**
+     *
+     * @Route("/new")
+     */
 	public function new(Request $request) {
 		// creates a task and gives it some dummy data for this example
 		$task = new Task();
 		$task->setTask('Write a blog post');
-		$task->setDueDate(new \DateTime('tomorrow'));
+// 		$task->setDueDate(new \DateTime('tomorrow'));
 		
 		$form = $this->createFormBuilder($task)
 					->add('task', TextType::class)
-					->add('dueDate', DateType::class)
+					->add('dueDate', DateType::class, array('widget' => 'single_text', 'required' => false))
 					->add('save', SubmitType::class, array (
 							'label' => 'Create Task'))
 					->getForm();
@@ -40,7 +46,7 @@ class DefaultController extends Controller {
 			return $this->redirectToRoute('task_success');
 		}
 		
-		return $this->render('default/new.html.twig', array(
+		return $this->render('form/new.html.twig', array(
 				'form' => $form->createView(),
 		));
 	}
